956501c5f43b771c67673b2f6a607e63abdf0d27
Timothy Hoang <timh1203@gmail.com>
2019-03-21 18:59:49 -0600

    Add routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // const wes = { name: 'Wes', age: 100, cool: true };
+  // console.log('Hey!!');
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  // res.send(req.query.name);
+  res.json(req.query);
+  res.render('hello');
 });
+
+router.get('/reverse/:name', (req, res) => {
+  // res.send('it works!');
+  // res.send(req.params.name)
+  const reversed = [...req.params.name].reverse().join('');
+  res.send(reversed)
+})
 
 module.exports = router;
