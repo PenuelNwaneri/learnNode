7e1adbc6ef3d94d9bf78d17bf3d7aabf2b80d9bd
Timothy Hoang <timh1203@gmail.com>
2019-03-25 18:17:29 -0600

    Add middleware and error handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,14 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Wes'; // passed down to the next function
+  if (req.name === 'Wes') {
+    throw Error('That is a stupid name'); // helps throw a stack trace for debugging
+  }
+
+  res.cookies('name', 'Wes is cool', { maxAge: 9000000 }); // sets cookies with cookieParser
+  next();
+}
+
 exports.homePage = (req, res) => {
+  console.log(req.name); // can access the local middleware
   res.render('index');
 }
