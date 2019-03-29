7a2b20c4d1fe7f32d0ae6d3d2ae772ab6386514d
Timothy Hoang <timh1203@gmail.com>
2019-03-29 17:14:15 -0600

    Add flash message

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
+  req.flash('', '');
   res.render('index');
 }
 
@@ -10,9 +11,9 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 }
 
 // exports.myMiddleware = (req, res, next) => {
