cc85cd98c78227174d88a5bc3a38fac607ab04aa
Timothy Hoang <timh1203@gmail.com>
2019-03-29 18:03:35 -0600

    Add query db for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -16,6 +16,11 @@ exports.createStore = async (req, res) => {
   res.redirect(`/store/${store.slug}`);
 }
 
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+}
+
 // exports.myMiddleware = (req, res, next) => {
 //   req.name = 'Wes'; // passed down to the next function
 //   if (req.name === 'Wes') {
